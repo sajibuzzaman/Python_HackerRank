b2969b8f79433cab4633509f8c7b7d873c2dd141
Sajib Uzzaman <muhammadsajibuzzaman1998@gmail.com>
2022-03-21 23:05:51 +0600

    Update Designer Door Mat.py

--- Designer Door Mat.py
+++ Designer Door Mat.py
@@ -1,13 +1,23 @@
-# Enter your code here. Read input from STDIN. Print output to STDOUT
-I = input().split()
-N = int(I[0])
-M = int(I[1])
-X = ".|."
+# # Enter your code here. Read input from STDIN. Print output to STDOUT
+# I = input().split()
+# N = int(I[0])
+# M = int(I[1])
+# X = ".|."
+#
+# for i in range(N//2):
+#     print(((X*i) + X + (X*i)).center(M, '-'))
+#
+# print('WELCOME'.center(M, '-'))
+#
+# for i in range(N//2):
+#     print(((X * ((N//2) - 1 - i)) + X + (X * ((N//2) - 1 - i))).center(M, '-'))
 
-for i in range(N//2):
-    print(((X*i) + X + (X*i)).center(M, '-'))
+# Enter your code here. Read input from STDIN. Print output to STDOUT
+N, M = map(int, input().split())
+for i in range(1, N, 2):
+    print((i * ".|.").center(M, '-'))
 
 print('WELCOME'.center(M, '-'))
 
-for i in range(N//2):
-    print(((X * ((N//2) - 1 - i)) + X + (X * ((N//2) - 1 - i))).center(M, '-'))
+for i in range(N-2, -1, -2):
+    print((i * ".|.").center(M, '-'))
