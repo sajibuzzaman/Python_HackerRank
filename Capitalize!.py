0b60f5f1c4f37b2ddd59869de8c65b3a8b6f3f35
Sajib Uzzaman <muhammadsajibuzzaman1998@gmail.com>
2022-03-22 19:27:04 +0600

    Create Capatalize

---
+++ Capitalize!.py
@@ -0,0 +1,25 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+# Complete the solve function below.
+def solve(s):
+    # return s.title()
+    for i in s.split():
+        s = s.replace(i, i.capitalize())
+    return s
+if __name__ == '__main__':
+    os.environ['OUTPUT_PATH'] = 'output.txt'
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    s = input()
+
+    result = solve(s)
+
+    fptr.write(result + '\n')
+
+    fptr.close()
