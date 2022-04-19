43b6d89ab04f9015426b681746b83b4d9e21fa87
Sajib Uzzaman <muhammadsajibuzzaman1998@gmail.com>
2022-04-19 19:08:34 +0600

    Create Power - Mod Power.py

---
+++ Power - Mod Power.py
@@ -0,0 +1,28 @@
+'''
+Task
+You are given three integers: a, b, and m. Print two lines.
+On the first line, print the result of pow(a,b). On the second line, print the result of pow(a,b,m).
+
+Input Format
+The first line contains a, the second line contains b, and the third line contains m.
+
+Sample Input
+
+3
+4
+5
+Sample Output
+
+81
+1
+'''
+
+# Enter your code here. Read input from STDIN. Print output to STDOUT
+
+if __name__ == '__main__':
+    a = int(input())
+    b = int(input())
+    m = int(input())
+
+    print(pow(a, b))
+    print(pow(a, b, m))
